cfdb8ddd802fdf112820f44ff87499cd4915585a
Mitchell Slep <mitch@moomoobloo.com>
2017-06-10 13:25:33 -0700

    BLE peripheral that implements CGM service. Adapted from https://github.com/sandeepmistry/bleno/tree/master/examples/battery-service.

---
+++ bin/oref0-ble-peripheral-main.js
@@ -0,0 +1,24 @@
+var bleno = require('../..');
+var CgmService = require('../lib/ble/cgm-service');
+
+var primaryService = new CgmService();
+
+bleno.on('stateChange', function(state) {
+    console.log('on -> stateChange: ' + state);
+
+    if (state === 'poweredOn') {
+	bleno.startAdvertising('OpenAPS', [primaryService.uuid]);
+    } else {
+	bleno.stopAdvertising();
+    }
+});
+
+bleno.on('advertisingStart', function(error) {
+    console.log('on -> advertisingStart: ' + (error ? 'error ' + error : 'success'));
+
+    if (!error) {
+	bleno.setServices([primaryService], function(error){
+	    console.log('setServices: '  + (error ? 'error ' + error : 'success'));
+	});
+    }
+});
